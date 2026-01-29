e1be8d9a0f68954150fbc3a853f77da80019b6c5
feminite-diva <kpysanenko@huspi.com>
2026-01-29 20:27:03 +0200

    Homework 12 (edited 12.2)

--- ДЗ 12/12.2/js/script12-2.js
+++ ДЗ 12/12.2/js/script12-2.js
@@ -1,6 +1,6 @@
 const container = document.querySelector('#container');
 
-parent.addEventListener('click', (e) => {
+container.addEventListener('click', (e) => {
   if (e.target.tagName === 'BUTTON') {
     alert(`You clicked ${e.target.textContent}`);
   }
