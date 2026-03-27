ce0dfb12885d74c6990b085651f72a190d3356b3
feminite-diva <kpysanenko@huspi.com>
2026-03-27 18:18:49 +0200

    Homework 27 (edited content)

--- ДЗ 27/ДЗ 27.1/spa app/src/pages/About.jsx
+++ ДЗ 27/ДЗ 27.1/spa app/src/pages/About.jsx
@@ -3,9 +3,10 @@ import TodoList from '../components/TodoList';
 const Home = () => {
   return (
     <div className="page-content">
-      <h1>Головна сторінка</h1>
-      <p>Тут ви можете керувати своїми справами:</p>
-      <TodoList />
+      <h1>Про мене</h1>
+       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dui magna, tristique at ex varius, viverra consequat mi. Proin ullamcorper ac tellus ac pretium. Nullam at tempus sem. Vestibulum efficitur est eu diam efficitur cursus. Suspendisse auctor eget lectus ut efficitur. Sed non blandit neque, a consectetur ligula. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam egestas leo elit, placerat consectetur eros congue non. Nunc urna sapien, lacinia et elit sodales, hendrerit lacinia eros. Ut gravida, enim ac dignissim placerat, metus nisl vehicula sem, eget vestibulum arcu mauris sit amet urna. Duis ut tortor eget mi molestie suscipit. Mauris turpis ligula, tempor accumsan diam eu, ornare elementum tellus. Duis mattis sodales nulla at pretium. Cras consequat nunc nec nisl tempor interdum.
+
+       <p>Aliquam id urna faucibus, gravida turpis non, laoreet metus. Cras tincidunt, ante at imperdiet bibendum, libero enim congue libero, ut convallis eros lorem sed risus. Aliquam commodo, massa at venenatis molestie, dolor risus malesuada tellus, rhoncus semper felis tortor at magna. Suspendisse potenti. Etiam vitae convallis mi. Nam nec justo pharetra, lobortis dolor ac, varius velit. Proin euismod vitae lectus in cursus. Quisque cursus vitae magna sit amet elementum. Donec porta dignissim lacus. </p>
     </div>
   );
 };
